b38048791929e6c608407e82dafbb5307233ec0e
Phil <pespinoza@missiondata.com>
2018-06-28 20:50:24 -0400

    Updates to rails script, add scratch file to whitelist

--- rails.sh
+++ rails.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 # quit on unexpected error
-set -e
-# set up rbenv
-rbenv init
+# set -e
+set -x
+# prep rbenv
+brew update
+for package in "rbenv" "ruby-build" "postgresql"; do
+    echo "Here"
+    eval "brew outdated | grep -q $package && brew upgrade $package" || 1
+done
 # set up latest ruby version
-rbenv install -l | grep -v - | tail -1
-# set up bundler
-# gem install bundler
-# bundle install
-# rails new (appname) -d postgresql
-# bundle add pg
+for command in "install" "local"; do
+    eval "rbenv $command \$(rbenv install -l | grep -v - | tail -1)"
+done
+ruby -v
+# set up rails app using first argument as app name
+gem install rails 
+rails new "$1" -d -T postgresql
+# cd "$1"
+# set up bundler for app
+gem install bundler
+bundle install
 # add basic gems with bundler
-# bundle add database_cleaner
-# bundle add factory_bot_rails
-# bundle add faker
-# bundle add rspec-rails
-# bundle add rubocop
-# bundle add guard
+bundle add database_cleaner
+bundle add factory_bot_rails
+bundle add faker
+bundle add rspec-rails
+bundle add rubocop
+bundle add guard
+bundle add guard-rails
+bundle add rspec-rails
+bundle install
 # set up rspec
-#   rspec-rails
-#   rails generate rspec:install
-# setup factory bot linter
-# setup rubocop
-?bundle guard init
+rails g rspec:install
 # setup guard
+bundle exec guard init
+bundle guard init rails
+# setup rubocop
 # set up graphql
 #   rails generate graphql:install
-
